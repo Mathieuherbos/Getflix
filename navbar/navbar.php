c018de3d210150ef9f90ca91fa5dc888e0bfb2af
unknown <elkhadrik6@gmail.com>
2020-08-26 14:41:59 +0200

    header & footer

---
+++ navbar/navbar.php
@@ -0,0 +1,47 @@
+
+  
+    <nav class="navbar navbar-expand-sm fixed-top navbar-light bg-default">
+  <a class="navbar-brand" href="#">Catflix</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Movies
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Box Office</a>
+                <a class="dropdown-item" href="#">Comedy</a>
+                <a class="dropdown-item" href="#">Thriller</a>
+                
+            </li>
+            </ul>
+    <!--search bar-->
+        <form class="form-inline my-2 my-lg-0">
+        <!-- Actual search box -->
+            <div class="form-group has-search">
+                <span class="fa fa-search form-control-feedback"></span>
+                <input type="text" class="form-control" placeholder="Search kitty kitty...">
+            </div>
+        </form>
+        <!--User profile link-->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">Welcome, User
+                </a>
+                <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                <a class="dropdown-item" href="#"><i class="fa fa-cog"></i>Preferences</a>
+                <a class="dropdown-item" href="#"><i class="fa fa-envelope"></i>Contact support</a>
+                <a class="dropdown-item" href="#"><i class="fa fa-power-off"></i>Logout</a>
+                </div>
+            </li>
+    </nav>  
+</header>
+<!--/.Navbar/Header -->
