8919fed745f0deccbaa2f4ed2f8a6173be09d39b
Nicolas Saeys <nicolas.saeys@gmail.com>
2020-08-26 11:54:03 +0200

    visual update

--- login.php
+++ login.php
@@ -73,20 +73,23 @@
     <title>LOGIN</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css"/>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
 </head>
-<body>
 
-<div class="jumbotron vertical-center login">
-            <div class="container-fluid">
-                
-                <div class="row align-items-center">
-                    
-                    <div class="col-sm-6 offset-sm-2 col-md-4 offset-md-4 mx-auto text-center signinform">
-                        <h1 class="title"><b>LOG IN</b></h1>
-                            <form action="login.php" method="post">
+
+
+
+    <body>
+        <section id="cover" class="min-vh-100">
+            <div id="cover-caption">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-10 mx-auto text-center form p-4">
+                            <h1 class="title">SIGN IN</h1>
+                            <div class="px-2 signinform">
+                                <form action="login.php" method="post">
                                     <div class="usernameSi">
                                         <p>User name</p>
                                     </div>
@@ -97,16 +100,15 @@
                                 <input type="password" name="pw" placeholder="">
                                 <br>
                                 <button type="submit" name="submit" class="btn btn-outline-light gobtn"><b>GO ! <i class="fa fa-paw animate__animated animate__fadeIn" aria-hidden="true"></i></b></button>
-                            </form>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+    </body>
+</html>
     
 
-
-
-</body>
-
-</html>
 
--- signup.php
+++ signup.php
@@ -23,19 +23,19 @@ header("location: login.php")
     <title>SIGN UP</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css"/>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
 </head>
 <body>
 
-<div class="jumbotron vertical-center signup">
-            <div class="container-fluid">
-                
-                <div class="row align-items-center">
-                    
-                    <div class="col-sm-8 offset-sm-4 col-md-4 offset-md-4 mx-auto text-center signupform">
-                        <h1 class="title"><b>SIGN UP </b></h1>
+        <section id="cover2" class="min-vh-100">
+            <div id="cover-caption">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-10 mx-auto text-center form p-4">
+                            <h1 class="title">SIGN UP</h1>
+                            <div class="px-2 signinform">
                             <form action="login.php" method="post">
                                     <div class="usernameSi">
                                         <p>First name</p>
@@ -60,10 +60,12 @@ header("location: login.php")
                                 <br>
                                 <button type="submit" name="submit" class="btn btn-outline-light gobtn"><b>GO ! <i class="fa fa-paw animate__animated animate__fadeIn" aria-hidden="true"></i></b></button>
                             </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     
 
 
