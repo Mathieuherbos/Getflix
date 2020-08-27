a73b60f2289c29899126f6484851fe085fd62721
Mathieu Herbos <mathieu.herbos@gmail.com>
2020-08-27 14:16:10 +0200

    presque fin streaming page

--- streaming.php
+++ streaming.php
@@ -7,17 +7,32 @@
     </head>
 
     <body>
-                    <div class="container">
-                        <?php
-                            $bdd = new PDO ('mysql:host=localhost;port=3308;dbname=getflixtest','root','root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                            $reponse = $bdd->query ('SELECT * FROM youtube');
-                            while ($datas = $reponse -> fetch())
-                            {
-                                echo 
-                                    '<iframe class="responsive-iframe" src="' . $datas['Link'] . '">
-                                    </iframe>';
-                            }
-                        ?>               
-                    </div>
+        <div class="container">
+            <?php
+                $bdd = new PDO ('mysql:host=localhost;port=3308;dbname=getflixtest','root','root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                $reponse = $bdd->query ('SELECT * FROM youtubeinfos');
+                while ($datas = $reponse -> fetch())
+                {
+                    echo 
+                        '<iframe class="responsive-iframe" src="' . $datas['link'] . '">
+                        </iframe>';
+                }
+            ?>               
+        </div>
+        <div class="container-fluid" id="description">
+            <?php
+                $reponse = $bdd->query ('SELECT * FROM youtubeinfos');
+                while ($datas = $reponse -> fetch())
+                        {
+                            echo 
+                                '<h2>' . $datas['title'] . '</h2>
+                                <p>' . $datas['description'] . '</p>
+                                <div id="type"><h5>Type : ' . $datas['types'] . ' </h5></div>
+                                <div id="duration"><h5>Duration : ' . $datas['duration'] . ' min </h5></div>
+                                <div id="release"><h5>Release date : ' . $datas['releaseDate'] . '</h5></div>
+                                <div id="rating"><h5>Rating : ' . $datas['rating'] . '</h5></div>';
+                        }
+            ?>
+        </div>
     </body>
 </html>
