87f0d50d2192fdf6123c01d105c5f40ac59d00fd
unknown <elkhadrik6@gmail.com>
2020-08-27 10:35:23 +0200

    Update final footer

--- footer/footer.php
+++ footer/footer.php
@@ -2,11 +2,11 @@
 <footer class="footer fixed-bottom font-small special-color-dark">
   <!-- Footer Elements -->
   <div class="container">
-    <div class="row">
+    
+    <div class="row align-items-center">
       <!-- Grid column -->
-      <div class=" col-sm-4 col-md-12 py-5">
+      <div class=" col-sm-4 col-md-8 col-lg-12 pl-4">
         <div class="flex-center">
-
           <!-- Facebook -->
           <a class="fb-ic">
             <i class="fa fa-facebook-f fa-lg text-white mr-md-5 mr-3 fa-2x"> </i>
@@ -22,42 +22,43 @@
         </div>
       </div>
       <!-- Grid column -->
-    </div>
-    <div class="col-sm-4 col-md-6 col-lg-12 mx-auto">
-    <!-- Links -->
-          <h5 class="font-weight-medium text-uppercase text-white mt-3 mb-4">Any questions?</h5>
-            <ul class="list-unstyled text-white">
-              <li>
-                <a href="#!" class="text-white">Call 0800-XXX-XX</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">FAQ</a>
-              </li>
-              <li>
-                <a href="#!" class="text-white">Terms of Use</a>
-              </li>
-            </ul>
-  </div>
-</div>
-    <!-- Grid column -->
-  <div class="col-sm-4 col-md-6 col-lg-8 mx-auto">
-      <!-- Grid column -->
-     <!--Grid column newsletter-->
-     <div class="col-sm-4 col-md-6 col-lg-8 mb-4">
-       <h6 class="font-weight-medium text-uppercase text-white">Susbcribe and win a premium account!</h6>
-        <form class="input-group flex-center">
-        <input type="text" class="form-control form-control-sm" placeholder="Your email"
-            aria-label="Your email" aria-describedby="basic-addon2">
-            <div class="input-group-append">
-                  <button class="btn btn-sm btn-outline-white text-white my-0" type="button">Sign up</button>
-            </div>
+      <div class="col-sm">
+        <div class="col-sm-4 col-md-8 col-lg-12">
+        <!-- Links -->
+            <h5 class="font-weight-medium text-uppercase text-white mt-5 mb-3">Any questions?</h5>
+              <ul class="list-unstyled text-white">
+                <li>
+                  <a href="#!" class="text-white">Call 0800-XXX-XX</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">FAQ</a>
+                </li>
+                <li>
+                  <a href="#!" class="text-white">Terms of Use</a>
+                </li>
+              </ul>
+        
+        </div>  
       </div>
-        </form>
-  </div>
 
+      <!-- Grid column -->
+      <div class="col-sm">
+        <!--Grid column newsletter-->
+        <div class="col-sm-4 col-md-8 col-lg-12">
+          <h6 class="font-weight-medium text-uppercase text-white">Susbcribe and win a premium account!</h6>
+            <form class="input-group flex-center">
+              <input type="text" class="form-control form-control-sm" placeholder="Your email"
+                aria-label="Your email" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                  <button class="btn btn-sm btn-outline-white text-white my-0" type="button">Sign up</button>
+              </div>
+            </form>
+        </div>
+      </div>
+  </div>
 
   <!-- Copyright -->
-  <div class="footer-copyright font-weight-light text-center text-white py-3">© 2020 Copyright:
+  <div class="footer-copyright font-weight-light small text-center text-white py-3">© 2020 Copyright:
     <a href="https://github.com/Nicolassaeys/Getflix"> Becode.org </a>
   </div>
   <!-- Copyright -->
